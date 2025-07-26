2e0ba6978116f885341f71ef0c5fdd7f81611c3a
Varun Raj <varun@skcript.com>
2025-07-26 09:30:07 +0530

    feat: update environment configuration and dependencies
    - Added DATABASE_URL to the environment configuration for improved database connectivity.
    - Updated .gitignore to include .env.local for better environment management.
    - Added new dependencies: @radix-ui/react-radio-group and date-fns for enhanced UI components and date handling.

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,5 @@
 export const ENV = {
-    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL!,
-    AUTH_SECRET: process.env.AUTH_SECRET!,
+  BASE_URL: process.env.NEXT_PUBLIC_BASE_URL!,
+  AUTH_SECRET: process.env.AUTH_SECRET!,
+  DATABASE_URL: process.env.DATABASE_URL!,
 }
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,11 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
+import { ENV } from '@/config/env';
 // You can specify any property from the node-postgres connection options
 export const db = drizzle({
   connection: {
-    connectionString: process.env.DATABASE_URL!,
+    connectionString: ENV.DATABASE_URL,
     ssl: false
   },
   schema: schema
